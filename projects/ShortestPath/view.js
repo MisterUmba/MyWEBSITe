f0dae667f3b0854ecec30f00d6fbdbb5d087de8c
Henri Umba <umbamw@gmail.com>
2023-04-29 10:27:26 -0400

    Formatted the file

--- projects/ShortestPath/view.js
+++ projects/ShortestPath/view.js
@@ -11,29 +11,29 @@ let newEdge = undefined;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight - 50;
 
-function clear(){
+function clear() {
     pen.fillStyle = "#000000";
-    pen.fillRect(0,0, canvas.width, canvas.height);
+    pen.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawNewEdge(b, e){
+function drawNewEdge(b, e) {
     newEdge = new Edge(b, e);
 }
 
-function resize(canvas){
+function resize(canvas) {
     // Lookup the size the browser is displaying the canvas.
     let dispW = canvas.clientWidth;
     let dispH = canvas.clientHeight;
 
     // Check if canvas is not same size
-    if (canvas.width !== dispW || canvas.height !== dispH){
+    if (canvas.width !== dispW || canvas.height !== dispH) {
         // Make the canvas the same size
         canvas.width = dispW;
         canvas.height = dispH;
     }
 }
 
-function draw(){
+function draw() {
     resize(canvas);
     clear();
     pen.save();
@@ -41,7 +41,7 @@ function draw(){
     pen.fillStyle = "white";
     pen.lineWidth = 10;
 
-    if(newEdge !== undefined){
+    if (newEdge !== undefined) {
         pen.save();
 
         pen.strokeStyle = "white";
@@ -55,11 +55,11 @@ function draw(){
     }
 
     // Drawing all Edges
-    for(let x = 0; x < Graph.length; x++){
-        for(let i = 0; i < Graph[x].Edges.length; i ++){
+    for (let x = 0; x < Graph.length; x++) {
+        for (let i = 0; i < Graph[x].Edges.length; i++) {
             let ed = Graph[x].Edges[i];
             pen.strokeStyle = "white";
-            if(ed.selected)
+            if (ed.selected)
                 pen.strokeStyle = "gold";
             pen.beginPath();
             pen.moveTo(ed.b.x, ed.b.y);
@@ -69,14 +69,14 @@ function draw(){
     }
 
     // Drawing edge cost
-    for(let x = 0; x < Graph.length; x++){
-        for(let i = 0; i < Graph[x].Edges.length; i ++){
+    for (let x = 0; x < Graph.length; x++) {
+        for (let i = 0; i < Graph[x].Edges.length; i++) {
             let ed = Graph[x].Edges[i];
             pen.fillStyle = "red";
             pen.textAlign = "center";
             let pos = {
-                x: Math.floor(Math.abs(ed.b.x + ed.e.x)/2),
-                y: Math.floor(Math.abs(ed.b.y + ed.e.y)/2)
+                x: Math.floor(Math.abs(ed.b.x + ed.e.x) / 2),
+                y: Math.floor(Math.abs(ed.b.y + ed.e.y) / 2)
             }
             pen.fillText(ed.cost, pos.x, pos.y);
         }
@@ -86,13 +86,13 @@ function draw(){
     pen.strokeStyle = "red";
     pen.fillStyle = "white";
     pen.textAlign = "center";
-    for(let x = 0; x < Graph.length; x++){
+    for (let x = 0; x < Graph.length; x++) {
         pen.beginPath();
-        pen.moveTo(Graph[x].x , Graph[x].y);
-        pen.arc(Graph[x].x , Graph[x].y, radius,0,2*Math.PI,false);
-        if(Graph[x].selected)
+        pen.moveTo(Graph[x].x, Graph[x].y);
+        pen.arc(Graph[x].x, Graph[x].y, radius, 0, 2 * Math.PI, false);
+        if (Graph[x].selected)
             pen.stroke();
-        if(Graph[x] === Graph.GOLD){
+        if (Graph[x] === Graph.GOLD) {
             pen.strokeStyle = "Gold";
             pen.stroke()
             pen.strokeStyle = "red";
@@ -101,7 +101,7 @@ function draw(){
     }
 
     // Drawing Node label
-    for(let x = 0; x < Graph.length; x++){
+    for (let x = 0; x < Graph.length; x++) {
         pen.fillStyle = "red";
         pen.fillText(x, Graph[x].x, Graph[x].y);
     }
