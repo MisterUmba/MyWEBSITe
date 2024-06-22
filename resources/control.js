c29141d0f6dcb54538bfe6f583a2ffd8d9db89d2
Henri Umba <umbamw@gmail.com>
2024-06-22 17:30:52 -0400

    format file

--- resources/control.js
+++ resources/control.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   // Controls Size of upper picture
   $("#head").height($(window).height());
 
   // on Resize
-  $(window).resize(function(){
-      $("#head").height($(window).height());
+  $(window).resize(function () {
+    $("#head").height($(window).height());
   });
 
   // Press button to play Snake Online version
-  $("#snake_game_javascript_version").click(function(e){
+  $("#snake_game_javascript_version").click(function (e) {
     e.preventDefault();
     window.location.href = "projects/snake/Snakegame.html"
   });
@@ -27,19 +27,19 @@ $(document).ready(function(){
   });
 
   // Downloading astroid game .jar files
-  $("#astroidGame").click(function(e){
+  $("#astroidGame").click(function (e) {
     e.preventDefault();
     window.location.href = "projects/Physics_Game.jar";
   });
 
   // Downloading KeyStroker exe file
-  $("#keystroke").click(function(e){
+  $("#keystroke").click(function (e) {
     e.preventDefault();
     window.location.href = "projects/KeyStroke.exe";
   });
 
   // Snake game
-  $("#snakeGame").click(function(e){
+  $("#snakeGame").click(function (e) {
     e.preventDefault();
     window.location.href = "projects/HenriSnake.jar";
   });
@@ -47,21 +47,21 @@ $(document).ready(function(){
 
 
   // tic tac toe game
-  $("#tictactoeGame").click(function(e){
+  $("#tictactoeGame").click(function (e) {
     e.preventDefault();
     window.location.href = "projects/Tic_Tac_Toe.jar";
   });
 
   // Make
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     $("#centerAnimation").css("margin-left", "40%");
     $("#tictactoeGame").hide();
     $("#snakeGame").hide();
     $("#astroidGame").hide();
   }
 
-  $('.nav a').on('click', function(){
-    if($(window).width() <= 767)
+  $('.nav a').on('click', function () {
+    if ($(window).width() <= 767)
       $('.navbar-toggle').click();
   });
 
