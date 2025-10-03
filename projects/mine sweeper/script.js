975636306cec007c04401ff907a1dfbd638ea690
Henri Umba <umbamw@gmail.com>
2025-10-03 00:02:05 -0400

    Using ENUM to define the current state of the game .
    Drawing will then happen based on the state of the game.

--- projects/mine sweeper/script.js
+++ projects/mine sweeper/script.js
@@ -1,4 +1,11 @@
-let GAME_MODE = 'menu';
+const GAME_STATES = {
+  MENU: 'menu',
+  MENU_OPTION: 'menu_options',
+  PLAYING: 'playing',
+  PLAYING_OPTION: 'game_difficulty'
+}
+
+let GAME_MODE = GAME_STATES.MENU;
 
 const canvas = document.getElementById("canvas");
 const pen = canvas.getContext('2d');
@@ -11,22 +18,38 @@ function clearScreen() {
 }
 
 function drawGame() {
-
+  console.log("playing the game. ")
 }
 
 function drawGameOptions() {
-
+  console.log("Choosing the game difficulty");
 }
 
 function drawMenu() {
-
+  console.log("On the Main Menu");
 }
 
 function drawMenuOptions() {
-
+  console.log("Picking menu options. (i.e. Muting sounds)");
 }
 
 
+function draw() {
+  switch (GAME_MODE) {
+    case GAME_STATES.PLAYING:
+      drawGame();
+      break;
+    case GAME_STATES.PLAYING_OPTION:
+      drawGameOptions();
+      break;
+    case GAME_STATES.MENU:
+      drawMenu();
+      break;
+    case GAME_STATES.MENU_OPTION:
+      drawMenuOptions();
+      break;
+  }
+}
 
 function resize() {
   canvas.width = window.innerWidth * 0.5;
