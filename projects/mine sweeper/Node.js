16e04bc76083dffd62c2667e72a85a292597eaec
Henri Umba <umbamw@gmail.com>
2025-10-04 22:53:22 -0400

    Created File for tracking elements on canvas

---
+++ projects/mine sweeper/Node.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(x, y, width, height, color = "white", text = "") {
+    this.x = x;
+    this.y = y;
+    this.w = width;
+    this.h = height;
+    this.color = color;
+    this.text = text;
+  }
+
+  draw(x = this.x, y = this.y, w = this.w, h = this.h, c = this.color, t = this.text) {
+    pen.save();
+    pen.fillStyle = c;
+    pen.fillRect(x, y, w, h);
+    pen.textAlign = 'center';
+    pen.textBaseline = 'middle';
+    pen.fillStyle = invertColor(pen.fillStyle);
+    pen.font = '25px sans-serif';
+    pen.fillText(t, x + .5 * w, y + .5 * h, w);
+  }
+}
+
+class ViewGrap {
+  constructor(nodes = []) {
+    this.nodes = nodes;
+  }
+
+  draw() {
+    this.nodes.forEach(node => node.draw());
+  }
+}
