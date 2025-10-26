5f34222d00cff9033b9f3df02a43b6be828c53af
Henri Umba <umbamw@gmail.com>
2025-10-26 16:13:44 -0400

    Created Model
    'This model represents the actual game and it'\''s status'

--- projects/mine sweeper/model.js
+++ projects/mine sweeper/model.js
@@ -32,3 +32,79 @@ class Cell {
   }
 }
 
+class Model {
+  constructor(column = 8, rows = 8, boom_rate = 15.6) {
+    this.grid = [];
+    this.columns = column;
+    this.rows = rows;
+    this.timer = 0;
+    this.status = "smile";
+    this.flag_count = 0;
+    this.boom_rate = boom_rate;
+  }
+
+  generate() {
+    // empty grid
+    this.grid.length = 0;
+
+    // generate grid by rows and columns
+    for (let r = 0; r < this.rows; r++) {
+      this.grid.push([]);
+
+      for (let c = 0; c < this.columns; c++) {
+        this.grid[r].push(new Cell(Cell_Status.HID, Cell_values.BLANK));
+      }
+    }
+
+    // Place the booms randomly
+    let numOfBooms = Math.ceil((this.boom_rate / 100) * (this.rows * this.columns));
+
+    while (numOfBooms > 0) {
+      const randx = Math.floor(Math.random() * this.columns);
+      const randy = Math.floor(Math.random() * this.rows);
+
+      if (this.grid[randy][randx].value == Cell_values.BLANK) {
+        this.grid[randy][randx].value = Cell_values.BOOM;
+        numOfBooms--;
+      }
+    }
+
+    // Generate boom neighbor number values
+    for (let r = 0; r < this.rows; r++) {
+      for (let c = 0; c < this.columns; c++) {
+        let counter = 0;
+        // for top row until bottom row
+        for (let ri = Math.max(0, r - 1); ri <= Math.min(r + 1, this.rows - 1); ri++) {
+          // for left column until right column
+          for (let ci = Math.max(0, c - 1); ci <= Math.min(c + 1, this.columns - 1); ci++) {
+            if (ri == r && ci == c) continue;
+            if (this.grid[ri][ci].value == Cell_values.BOOM) { counter++ }
+          }
+        }
+
+        if (this.grid[r][c].value === Cell_values.BOOM) {
+          continue;
+        } else {
+          this.grid[r][c].value = counter;
+        }
+
+      }
+    }
+  }
+
+  pick(row, column) {
+
+  }
+
+  toString() {
+    let temp = "";
+    for (let r = 0; r < this.rows; r++) {
+      for (let c = 0; c < this.columns; c++) {
+        temp += "\t" + this.grid[r][c].value;
+      }
+      temp += '\n';
+    }
+
+    return temp;
+  }
+}
