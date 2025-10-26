cae159987218112f0be68218faaa715d6f1aa683
Henri Umba <umbamw@gmail.com>
2025-10-26 16:10:58 -0400

    Created Cell
    Cell holds informations about what each position on the board does

--- projects/mine sweeper/model.js
+++ projects/mine sweeper/model.js
@@ -10,3 +10,25 @@ const Cell_values = {
   BLANK: 0,
   NUMBER: 1,
 }
+
+class Cell {
+  constructor(status, value) {
+    this.status = status;
+    this.value = value;
+  }
+
+  reveal() {
+    if (this.value == Cell_values.BOOM) {
+      this.status = Cell_Status.EXPLODE;
+    } else if (this.value == Cell_values.BLANK) {
+      this.status = Cell_Status.REVEAL;
+    } else {
+      this.status = Cell_Status.NUMBER;
+    }
+  }
+
+  flag() {
+    this.status = Cell_Status.FLAG;
+  }
+}
+
